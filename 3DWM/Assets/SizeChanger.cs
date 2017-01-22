b24eb679141a4e12e2d0469e62e8fa723663e451
Nozomi Matsuzawa <matsuzawa@ueda.info.waseda.ac.jp>
2017-01-22 16:45:35 +0900

    git add し忘れ

---
+++ 3DWM/Assets/SizeChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeChanger : MonoBehaviour {
+    public GameObject display;
+    public GameObject pair;
+    Rigidbody src_rb, dst_rb;
+
+    // Use this for initialization
+    void Start () {
+        src_rb = GetComponent<Rigidbody>();
+        dst_rb = pair.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float dist = Vector3.Distance(src_rb.position, dst_rb.position);
+        Debug.Log(dist);
+        Debug.Log(display.transform.localScale);
+        display.transform.localScale = new Vector3(dist, dist, dist);
+    }
+}
